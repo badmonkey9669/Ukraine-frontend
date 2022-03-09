5e18cda333b5a8442200ce464b401f9f1b3a12f1
badmonkey9669 <badmonkey9669@protonmail.com>
2022-03-10 08:47:10 +0900

    feat: web3modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import logo from "./logo.svg";
+import { Container, Image } from "react-bootstrap";
+import NavBar from "./components/NavBar";
+
+import youwillseeourfaces from "./images/youwillseeourfaces.jpg";
 
 function App() {
-  return <Container>hi</Container>;
+  return (
+    <Container>
+      <NavBar />
+      <Image
+        src={youwillseeourfaces}
+        alt="You will see our faces"
+        width={"100%"}
+      />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { Container, Navbar } from "react-bootstrap";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
+const providerOptions = {
+  /* See Provider Options Section */
+};
+
+const web3Modal = new Web3Modal({
+  network: "mainnet", // optional
+  cacheProvider: true, // optional
+  providerOptions, // required
+});
+
+function NavBar() {
+  const [address, setAddress] = useState<string>();
+  const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner>();
+
+  useEffect(() => {
+    async function connect() {
+      const instance = await web3Modal.connect();
+
+      const provider = new ethers.providers.Web3Provider(instance);
+      const signerGuy = await provider.getSigner();
+      const addr = await signerGuy.getAddress();
+      setSigner(signerGuy);
+      setAddress(addr);
+    }
+
+    connect();
+  }, []);
+
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">You Will See Our Faces</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">{address}</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
